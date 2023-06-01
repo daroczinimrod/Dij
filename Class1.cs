a51191cfc48a45e2e07acc3e50caff6253efe843
rodi <72067090+daroczinimrod@users.noreply.github.com>
2023-06-01 10:07:08 +0200

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Class1
+    {
+        int evszam;
+        string tipus;
+        string kNev;
+        string vNev;
+
+        public Class1(int evszam, string tipus, string kNev, string vNev)
+        {
+            this.evszam = evszam;
+            this.tipus = tipus;
+            this.kNev = kNev;
+            this.vNev = vNev;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        List<Class1> nobel = new List<Class1>();
+        static void Main(string[] args)
+        { 
+            StreamReader sr = new StreamReader(args[0]);
+            string[] data = sr.ReadLine().Split(';');
+            Console.WriteLine("4. feladat: ");  where(x => x.evszam == "2017") ;
+            Console.WriteLine("6. feladat: "); where(x => x.vNev == "Curie");
+            Console.WriteLine("7. feladat: "); where ;
+            sr.Close();              
+        }
+
+    }
+}
